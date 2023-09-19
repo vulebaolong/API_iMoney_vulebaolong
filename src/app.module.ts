0aa7187d204a16a3155d6f06ee20bc08de2fc20c
Vũ Lê Bảo Long <vulebaolong@gmail.com>
2023-09-19 16:59:42 +0700

    delete env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,8 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ConfigModule } from "@nestjs/config";
+import { ConfigModule } from '@nestjs/config';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -9,6 +10,7 @@ import { ConfigModule } from "@nestjs/config";
       envFilePath: '.env',
       isGlobal: true,
     }),
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
