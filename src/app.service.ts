27bc16d7f75664ea29094c069100f161efb5d598
Vũ Lê Bảo Long <vulebaolong@gmail.com>
2023-09-23 23:41:33 +0700

    fix api test

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Welcome API IMoney!';
+    return 'Welcome API IMoney! ';
   }
 }
